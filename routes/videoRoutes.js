277e33dc0ed97c7618f1d5de3823b143a2a07396
WilliamxJiang <williamjiang021@gmail.com>
2025-01-18 16:56:13 -0500

    Fixed FFmpeg processing and added normalization logic

--- routes/videoRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const videoController = require("../controllers/videoController");
-
-// POST /video/generate
-router.post("/generate", videoController.generateVideo);
-
-module.exports = router;
